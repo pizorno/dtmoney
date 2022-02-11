25d81b0e4c99adddfb134f79576ae223f9907a9e
Eduardo Pizorno <pizorno@gmail.com>
2022-02-11 17:06:50 -0300

    Adicionando modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,34 @@
-import logoImg from '../../assets/logo.svg'
-import { Container, Content } from './styles'
+import { useState } from 'react';
+import Modal from 'react-modal';
+import logoImg from '../../assets/logo.svg';
+import { Container, Content } from './styles';
 
 export function Header() {
-    return (
-        <Container>
-            <Content>
-                <img src={ logoImg } alt="dt money" />
-                <button type="button">Nova transação</button>
-            </Content>
-        </Container>
-    )
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt='dt money' />
+        <button type='button' onClick={handleOpenNewTransactionModal}>
+          Nova transação
+        </button>
+        <Modal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+        >
+          <h2>Cadastrar Transação</h2>
+        </Modal>
+      </Content>
+    </Container>
+  );
 }
